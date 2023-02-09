5041a61b1903370c6db6adbed98d29dbe1ffc67d
janezb10 <janezb10@gmail.com>
2023-02-09 20:17:21 +0100

    malo boljš

--- app.js
+++ app.js
@@ -3,53 +3,56 @@ const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
 
 const app = express();
-const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(__dirname));
-app.use(express.static("views"));
-
-
+// one day session
 const oneDay = 1000 * 60 * 60 * 24;
+
+//session middleware
 app.use(sessions({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
     saveUninitialized:true,
     cookie: { maxAge: oneDay },
-    resave: false 
+    resave: false
 }));
 
-const myusername = "user1";
-const mypassword = "mypassword";
+// parsing the incoming data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//serving public file
+app.use(express.static("./static"));
+
+//username and password
+const myusername = 'user'
+const mypassword = 'pass'
+
+// a variable to save a session
 var session;
 
-app.get("/", (req, res) => {
-    session = req.session;
-    if(session.userid) {
-        res.send("Welcome User <a href=\'/logout'>click to logout</a>");
-    } else {
-        res.sendFile('views/index.html', {root:__dirname});
-    }
+app.get('/',(req,res) => {
+    session=req.session;
+    if(session.userid){
+        res.sendFile('./strani/notr.html',{root:__dirname});
+    }else
+    res.sendFile('./strani/index.html',{root:__dirname})
 });
 
-app.post("/user", (req, res) => {
-    if(req.body.username == myusername && req.body.password == mypassword) {
-        session = req.session;
-        session.userid = req.body.username;
+app.post('/user',(req,res) => {
+    if(req.body.username == myusername && req.body.password == mypassword){
+        session=req.session;
+        session.userid=req.body.username;
         console.log(req.session)
-        res.send("Hey there, welcome <a href=\'/logout'>click to logout</a>");
-    } else {
-        res.sendFile('strani/wrong.html', {root:__dirname});
+        res.sendFile('./strani/notr.html',{root:__dirname});
+    }
+    else{
+        res.sendFile('./strani/wrong.html',{root:__dirname});
     }
 })
 
-app.get('/logout', (req, res) => {
+app.get('/logout',(req,res) => {
     req.session.destroy();
     res.redirect('/');
-})
+});
 
-
-app.listen(port, () => {
-    console.log('Listening on port', port)
-})
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
