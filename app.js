61cd356618a2744f8dd9a32daf3168a1c9626889
janezb10 <janezb10@gmail.com>
2023-02-10 11:25:25 +0100

    tuki ni vse v app.js- routes -dela

--- app.js
+++ app.js
@@ -1,57 +1,27 @@
 require('dotenv').config();
 const express = require('express');
-const cookieParser = require("cookie-parser");
-const sessions = require('express-session');
-
 const app = express();
+const routes = require("./routes/index");
 
-// one day session
-const oneDay = 1000 * 60 * 60 * 24;
-
-//session middleware
-app.use(sessions({
-    secret: "adfm34idfs0df4nvl33l0d",
-    saveUninitialized:true,
-    cookie: { maxAge: oneDay },
-    resave: false
-}));
-
-
+const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./static"));
 
-//username and password kasneje v sql
-const myusername = 'user'
-const mypassword = 'pass'
+app.use("/", routes);
 
-// a variable to save a session
-var session;
 
-app.get('/',(req,res) => {
-    session=req.session;
-    if(session.userid){
-        res.sendFile('./strani/notr.html',{root:__dirname});
-    }else
-    res.sendFile('./strani/index.html',{root:__dirname})
+// Global Error Handler.
+app.use((err, req, res, next) => {
+    console.log("error: " + err.stack);
+    console.log("error: " + err.name);
+    console.log("error: " + err.code);
+
+    res.status(500).json({
+        errorMessage: err.message || "Something went rely wrong",
+    });
 });
 
-app.post('/user',(req,res) => {
-    if(req.body.username == myusername && req.body.password == mypassword){
-        session=req.session;
-        session.userid=req.body.username;
-        console.log(req.session)
-        res.sendFile('./strani/notr.html',{root:__dirname});
-    }
-    else{
-        res.sendFile('./strani/wrong.html',{root:__dirname});
-    }
-})
-
-app.get('/logout',(req,res) => {
-    req.session.destroy();
-    res.redirect('/');
-});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
---
+++ routes/index.js
@@ -0,0 +1,54 @@
+express = require('express');
+router = express.Router();
+const mysql = require('mysql2');
+const sessions = require('express-session');
+// const db = require('../config/SQLConfig');
+
+
+// one day session
+const oneDay = 1000 * 60 * 60 * 24;
+
+//session middleware
+router.use(sessions({
+    secret: "adfm34idfs0df4nvl33l0d",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+
+
+//username and password kasneje v sql
+const myusername = 'user'
+const mypassword = 'pass'
+
+// a variable to save a session
+var session;
+router.get('/',(req,res) => {
+    session=req.session;
+    if(session.userid){
+        res.sendFile('./strani/notr.html',{root:__dirname});
+    }else
+    res.sendFile('./strani/index.html',{root:__dirname})
+});
+
+router.post('/user',(req,res) => {
+    if(req.body.username == myusername && req.body.password == mypassword){
+        session=req.session;
+        session.userid=req.body.username;
+        console.log(req.session)
+        res.sendFile('./strani/notr.html',{root:__dirname});
+    }
+    else{
+        res.sendFile('./strani/wrong.html',{root:__dirname});
+    }
+})
+
+router.get('/logout',(req,res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+
+
+
+module.exports = router;
